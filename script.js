ee69c89d6f43a2aeeccc21d42a09e03b3a964bf4
Umar Khalid <umar.qur105@gmail.com>
2021-12-22 22:29:00 +0500

    updated

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
 	// typing text animation script
 	var typed = new Typed(".typing", {
-		strings: ["Freelancer", "Frontend Developer", "Programmer"],
+		strings: ["Freelancer", "Web Developer", "Programmer"],
 		typeSpeed: 100,
 		backSpeed: 60,
 		loop: true,
 	});
 
 	var typed = new Typed(".typing-2", {
-		strings: ["Freelancer", "Frontend Developer", "Programmer"],
+		strings: ["Freelancer", "Web Developer", "Programmer"],
 		typeSpeed: 100,
 		backSpeed: 60,
 		loop: true,
